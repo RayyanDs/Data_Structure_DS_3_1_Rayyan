16919a23ae07e658bf2d410ba2a604b33f7a14dc
Rayyan <muhammadrayyan2580@gmail.com>
2026-09-23 16:10:44 +0500

    Queue

---
+++ QueueArray/src/main/java/com/mycompany/queuearray/QueueArray.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.queuearray;
+
+/**
+ *
+ * @author MUHAMMAD RAYYAN
+ */
+public class QueueArray {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
